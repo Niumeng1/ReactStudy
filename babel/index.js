11b883c9b5d1f91c991171934ede481bd0f3a63b
snowflake <875347807@qq.com>
2019-04-26 18:33:57 +0800

    fetch练习

--- babel/index.js
+++ babel/index.js
@@ -1,1 +1,15 @@
-let foo = () => 'foo';
+//let foo = () => 'foo';
+async function test(key){
+    await fetch('./data'+key+'.json').then(res=>{
+        let resdata = res.json();
+         
+       
+            return resdata;
+    }).then(data=>{
+         console.log(data)
+    })
+}
+for (let index = 0; index <=1; index++) {
+    test(index)
+    
+}
